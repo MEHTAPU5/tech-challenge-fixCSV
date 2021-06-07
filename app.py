5ecb88e17cb1cca304511bc9ceed3fbf135c0b8f
Mehta <MEHTAPU5@novartis.net>
2021-06-07 13:18:17 +0530

    added-text-csv-corrupted

--- app.py
+++ app.py
@@ -9,20 +9,24 @@ import base64
 #  Fix CSV encoding 
 """
 
+
 def get_table_download_link(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}" download="fixedCSV.csv" target="_blank"><button style="padding: 5px; color: white; background: #73bcf0; border: 1px white; outline: 1px white;">DOWNLOAD FIXED CSV FILE</button></a>'
+    csv = df.to_csv(encoding='utf-8-sig', index=False)
+    # encoding the processed CSV with utf-8-sig and making it available for download
+    enc = csv.encode('utf-8-sig').decode()
+    href = f'<a href="data:file/csv;enc,{enc}" download="fixedCSV.csv" target="_blank"><button style="padding: 5px; color: white; background: #73bcf0; border: 1px white; outline: 1px white;">DOWNLOAD FIXED CSV FILE</button></a>'
     return href
 
-inputCSV =  st.file_uploader("Upload CSV", type=['csv'], accept_multiple_files=False, key="inputCSV", help="Input CSV file to fix encoding issues")
+
+inputCSV = st.file_uploader("Upload CSV", type=[
+                            'csv'], accept_multiple_files=False, key="inputCSV", help="Input CSV file to fix encoding issues")
 if inputCSV is not None:
-    
-    st.write("### Uploaded File : ",inputCSV.name)
-    df = pd.read_csv(inputCSV)
-    st.write("## File as CSV: ")
-    st.write(df)
-    st.write("## File as a Streamlit Table:")
+
+    st.write("### Uploaded File : ", inputCSV.name)
+
+    # after-encoding
+    df = pd.read_csv(inputCSV, encoding='utf-8')
+    st.write("## Fixed CSV File :")
     st.table(df)
+    #df.to_csv("fixedCSV.csv", encoding='utf-8-sig', index=False)
     st.markdown(get_table_download_link(df), unsafe_allow_html=True)
-    
